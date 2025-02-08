9bb0907cc9bd068ad85c4f7e2ab30707751abfbb
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-08 21:26:17 +0100

    init project

---
+++ src/main/java/io/michaeljgkopp/github/cruddemo/CruddemoApplication.java
@@ -0,0 +1,13 @@
+package io.michaeljgkopp.github.cruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/michaeljgkopp/github/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.michaeljgkopp.github.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
