b0a813b8e5ce9008b16fba4cf5152876899b6688
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-11 09:42:45 +0100

    delete a specific instructor

--- src/main/java/io/michaeljgkopp/github/cruddemo/CruddemoApplication.java
+++ src/main/java/io/michaeljgkopp/github/cruddemo/CruddemoApplication.java
@@ -19,19 +19,28 @@ public class CruddemoApplication {
 	public static CommandLineRunner commandLineRunner(InstructorRepository instructorRepository) {
 
 		return args -> {
-			instructorRepository.save(
-					Instructor.builder()
-							.firstName("Michael")
-							.lastName("Kopp")
-							.email("example@gmail.com")
-							.instructorDetail(
-									InstructorDetail.builder()
-											.hobby("coding")
-											.youtubeChannel("https://www.youtube.com")
-											.build()
-							)
-							.build()
-			);
+//			instructorRepository.save(
+//					Instructor.builder()
+//							.firstName("Michael")
+//							.lastName("Kopp")
+//							.email("example@gmail.com")
+//							.instructorDetail(
+//									InstructorDetail.builder()
+//											.hobby("coding")
+//											.youtubeChannel("https://www.youtube.com")
+//											.build()
+//							)
+//							.build()
+//			);
+
+			int id = 2;
+			Instructor instructor = instructorRepository.findById(id).orElse(null);
+			System.out.println("findById(1): " + instructor + "\n");
+
+			if (instructor != null) {
+				instructorRepository.deleteById(id);
+				System.out.println("Deleted instructor with id %d\n".formatted(id));
+			}
 
 			System.out.println("Instructors\n" +
 					"=".repeat(20));
